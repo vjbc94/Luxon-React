938804737d0a7810c0b49da950d627c579b6278e
victor.bolivar <vjbc94@gmail.com>
2022-05-24 22:15:33 -0300

    Componentes II

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,9 +2,27 @@ import './Card.css'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 
-const CardItem = ( {imagen, titulo, precio }) => {
+
+const CardItem = ( {imagen, titulo, precio, stock }) => {
+
+const [count, setCount ] = useState(0)
+
+    const sumQ = () => {
+        console.log("stock: ", stock)
+        if(count < stock) {
+            setCount(count + 1)
+        }
+    }
+    const restQ = () => {
+        if(count > 0) {
+            setCount(count - 1)
+        }
+    }
+
+
     return (
         <Card sx={{ minWidth: 275 }}>
             <CardContent>
@@ -14,6 +32,11 @@ const CardItem = ( {imagen, titulo, precio }) => {
                     </div>
                     <p>{titulo}</p>
                     <span>$ {precio}</span>
+                    <div className='contador'>
+                        <Button onClick= {restQ}> - </Button>
+                        <p> {count}</p>
+                        <Button onClick= {sumQ}> + </Button>
+                    </div>
                     <Button variant="outlined"> Comprar </Button>
                 </div>
             </CardContent>
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -6,55 +6,67 @@ const CardList = ({ titulo }) => {
         {
             titulo: 'Ryzen 5',
             precio: 36000,
-            imagen: 'raizen.png'
+            imagen: 'raizen.png',
+            stock: 7,
+            id: 1
         },
         {
             titulo: 'Grafica Geforce',
             precio: 95000,
-            imagen: 'gforce.jpg'
+            imagen: 'gforce.jpg',
+            stock: 7,
+            id: 2
         },
         {
             titulo: 'Disco SSD',
             precio: 7500,
-            imagen: 'ssd.jpg'
+            imagen: 'ssd.jpg',
+            stock: 7,
+            id: 3
         },
         {
             titulo: 'Motherboard',
             precio: 15000,
-            imagen: 'asus-600.png'
+            imagen: 'asus-600.png',
+            stock: 7,
+            id: 4
         },
         {
             titulo: 'Gabinete',
             precio: 8000,
-            imagen: 'gabinete.jpg'
+            imagen: 'gabinete.jpg',
+            stock: 7,
+            id: 5
         },
         {
             titulo: 'Fuente de Alimentacion',
             precio: 12000,
-            imagen: 'redragon.jpg'
+            imagen: 'redragon.jpg',
+            stock: 7,
+            id: 6
         },
     ]
     return (
         <>
             <h2>{titulo}</h2>
             <Grid container spacing={4} >
-                <Grid item md={4} >
-                    <CardItem titulo={"Ryzen"} precio={36000} imagen={"raizen.png"} />
+                <Grid item md={4}>
+                    <CardItem titulo={"Ryzen"} precio={36000} imagen={"raizen.png"} stock={7} id={1} />
                 </Grid>
                 <Grid item md={4}>
-                    <CardItem titulo={"Grafica Geforce"} precio={95000} imagen={"gforce.jpg"} />
+                    <CardItem titulo={"Grafica Geforce"} precio={95000} imagen={"gforce.jpg"} stock={7} id={2}/>
                 </Grid>
                 <Grid item md={4}>
-                    <CardItem titulo={"Disco Kingston"} precio={7500} imagen={"ssd.jpg"} />
+                    <CardItem titulo={"Disco Kingston"} precio={7500} imagen={"ssd.jpg"} stock={7} id={3} />
                 </Grid>
                 <Grid item md={4}>
-                    <CardItem titulo={"Motherboard Asus 600"} precio={15000} imagen={"asus-600.png"} />
+                    <CardItem titulo={"Motherboard Asus 600"} precio={15000} imagen={"asus-600.png"} stock={7} id={4} />
                 </Grid>
                 <Grid item md={4}>
-                    <CardItem titulo={"Gabinete Level-Up"} precio={8000} imagen={"gabinete.jpg"} />
+                    <CardItem titulo={"Gabinete Level-Up"} precio={8000} imagen={"gabinete.jpg"} stock={7} id={5} />
                 </Grid>
                 <Grid item md={4}>
-                    <CardItem titulo={"Fuente Alimentacion Redragon"} precio={12000} imagen={"redragon.jpg"} />
+                    <CardItem titulo={"Fuente Alimentacion Redragon"} precio={12000} imagen={"redragon.jpg"} stock={7}  id={6} />
                 </Grid>
             </Grid>
         </>
